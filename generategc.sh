f4e43af9d36db17b23ad3b3f5dff6c55b7138572
jy <jyegerlehner@yahoo.com>
2016-11-12 12:07:40 -0600

    adam->sgd->adam bad outcome.

--- generategc.sh
+++ generategc.sh
@@ -1,2 +1,2 @@
-python generate.py --samples 80000 --wav_out_path generated_p.wav --fast_generation true --gc_channels=32 --gc_id=311 --gc_cardinality=377 /media/mass1/logs/gc/train/2016-11-09T14-52-51/model.ckpt-6774
+python generate.py --samples 80000 --wav_out_path generated_p.wav --fast_generation false --gc_channels=32 --gc_id=311 --gc_cardinality=377 /media/mass1/logs/gc/train/2016-11-11T05-02-30/model.ckpt-44317
 
---
+++ resumegc.sh
@@ -0,0 +1,1 @@
+python train.py --data_dir=/media/mass1/audio_data/VCTK-Corpus --logdir=/media/mass1/logs/gc/train/2016-11-11T05-02-30 --learning_rate=0.0003 --optimizer=adam --gc_channels=32 --checkpoint_every=200
