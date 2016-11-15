57da445647405961bf09ce31ea0aa310b20f8b96
jy <jyegerlehner@yahoo.com>
2016-11-15 04:44:58 -0600

    model changes

--- generategc.sh
+++ generategc.sh
@@ -1,2 +1,2 @@
-python generate.py --samples 80000 --wav_out_path generated_q.wav --fast_generation false --gc_channels=32 --gc_id=311 --gc_cardinality=377 /media/mass1/logs/gc/train/2016-11-12T19-25-48/model.ckpt-116601
+python generate.py --samples 80000 --wav_out_path generated_q.wav --fast_generation false --gc_channels=32 --gc_id=311 --gc_cardinality=377 /media/mass1/logs/gc/train/2016-11-14T12-21-18/model.ckpt-71665
 
