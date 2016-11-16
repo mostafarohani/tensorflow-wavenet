41e65bc8754b73d76b602d7704ad927527beaec8
jy <jyegerlehner@yahoo.com>
2016-11-16 17:46:35 -0600

    scalar input init filter width 2.

--- traingc.sh
+++ traingc.sh
@@ -1,1 +1,1 @@
-python train.py --data_dir=/media/mass1/audio_data/VCTK-Corpus --logdir_root=/media/mass1/logs/gc --learning_rate=0.001 --optimizer=adam --momentum=0.9 --gc_channels=32 --checkpoint_every=1000
+python train.py --data_dir=/media/mass1/audio_data/VCTK-Corpus --logdir_root=/media/mass1/logs/gc --learning_rate=0.001 --optimizer=rmsprop --momentum=0.9 --gc_channels=32 --checkpoint_every=1000
